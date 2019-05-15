7779c2e00425fc0990a061c93d876fd177822825
AngelinaGarnica <angelina.garnica@gmail.com>
2019-05-15 00:31:40 -0300

    Agrega usuarios

--- TM_1.R
+++ TM_1.R
@@ -24,10 +24,18 @@ summary(tweets_junio[, c(2:4, 6:8, 11:13)])
 
 duplicados <- tweets_junio %>%
   group_by(id) %>%
-  mutate(cant = n())
-duplicados <- duplicados[duplicados$cant > 1 , ]
+  mutate(cant = n()) %>%
+  filter(cant > 1)
 
-length(unique(duplicados$id))
+#exactamente iguales
+
+dup <- duplicados %>% 
+  distinct()
+
+dup2 <- tweets_junio %>%
+  group_by_at(names(tweets_junio)[-grep("posicion", names(tweets_junio))]) %>%
+  mutate(cant = n()) %>%
+  filter(cant > 1)
 
 #Tomar desicion sobre estos casos (por ahora los dejo)
 
@@ -47,9 +55,9 @@ td <- tweets_junio %>%
 summary(td$cant)
 
 ggplot(td, aes("Tweets por día", cant)) +
-  geom_boxplot(show.legend = F) +
-  theme_classic()
-
+  geom_boxplot(fill = "grey65", width=0.1, show.legend = F) +
+  theme_classic() 
+  
 #Favoritos - Me gusta
 
 summary(tweets_junio$favorite_count)
@@ -59,16 +67,16 @@ summary(tweets_junio$favorite_count)
 nrow(tweets_junio[tweets_junio$favorite_count > 0, ])/nrow(tweets_junio)
 
 ggplot(tweets_junio, aes("Cantidad de me gusta", favorite_count)) +
-  geom_boxplot(show.legend = F) +
+  geom_boxplot(fill = "grey65", width=0.1, show.legend = F) +
   theme_classic()
 
 #porcentaje de tw con al menos 1 retw:
 
 summary(tweets_junio$retweet_count)
-nrow(tweets_junio[tweets_junio$retweet_count > 0, ])/nrow(tweets_junio$retweet_count)
+nrow(tweets_junio[tweets_junio$retweet_count > 0, ])/nrow(tweets_junio)
 
 ggplot(tweets_junio, aes("Cantidad de retweet", retweet_count)) +
-  geom_boxplot(show.legend = F) +
+  geom_boxplot(fill = "grey65", width=0.1, show.legend = F) +
   theme_classic()
 
 ggplot(tweets_junio, aes(x=retweet_count)) +
@@ -86,11 +94,29 @@ tu <- tweets_junio %>%
 nrow(tu)
 summary(tu$cant)
 
-ggplot(tu, aes("Tweets por usuario", cant)) +
-  geom_boxplot(show.legend = F) +
-  theme_classic()
-
 ggplot(tu, aes(x=cant)) +
-  geom_histogram(stat = "count", bins = 1000) + 
+  geom_histogram(stat = "count", bins = 30) + 
   xlab("usuarios") +
   theme_classic() 
+
+ggplot(tu, aes("Tweets por usuario", cant)) +
+  geom_boxplot(fill = "grey65", width=0.1, show.legend = F) +
+  theme_classic()
+
+##USUARIOS
+
+us_junio <- read.csv("C:/Users/User02/Google Drive/DiploDatos/Mentoria/users_junio.csv", 
+                         encoding = 'latin1')
+
+us_junio <- merge(us_junio, tu, by.x = "id", by.y = "user_id", all.x = T)
+
+tweets_junio$created_at <- as.character(tweets_junio$created_at)
+
+Sys.setlocale("LC_TIME", "English")
+us_junio$fecha <- as.POSIXct(us_junio$created_at, format="%a %b %d %H:%M:%S +0000 %Y", tz="GMT")
+us_junio$dia <- as.Date(us_junio$fecha)
+us_junio$year <- format(us_junio$dia, "%Y")
+
+summary(us_junio[, c(4,5,7,8,11,12,15)])
+
+                 
